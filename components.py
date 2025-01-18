9edb3d8bfc28bb68ed5b44105fe153687304defc
Team Developer <hybridhornets5150@gmail.com>
2025-01-18 16:39:42 -0500

    Added Closed_Loop pid controller config

--- components.py
+++ components.py
@@ -1,5 +1,6 @@
 import rev
-from rev import SparkMax, SparkMaxConfig, SparkBaseConfig, SparkBase, SparkLowLevel, SparkClosedLoopController
+from rev import SparkMax, SparkMaxConfig, SparkBaseConfig, SparkBase, SparkLowLevel, SparkClosedLoopController, \
+    ClosedLoopConfig
 
 from helpers import Component
 from collections import namedtuple
@@ -12,19 +13,30 @@ class SwerveModule(Component):
 
     _swerve_controller: SparkClosedLoopController
     _drive_controller: SparkClosedLoopController
+    swerve_pid: MotorConfig
+    drive_pid: MotorConfig
+
     def execute(self):
         pass
     def setup(self):
         swerve_motor_config = SparkMaxConfig()
         swerve_motor_config.setIdleMode(SparkBaseConfig.IdleMode.kBrake).smartCurrentLimit(50)
-        self.swerve_motor.configure(swerve_motor_config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
+        swerve_motor_config.closedLoop.pid(self.swerve_pid.kP, self.swerve_pid.kI, self.swerve_pid.kD,
+                                           rev.ClosedLoopSlot.kSlot0)
+        self.swerve_motor.configure(swerve_motor_config, SparkBase.ResetMode.kResetSafeParameters,
+                                    SparkBase.PersistMode.kPersistParameters)
 
         drive_motor_config = SparkMaxConfig()
         drive_motor_config.apply(swerve_motor_config).setIdleMode(SparkBaseConfig.IdleMode.kCoast)
-        self.drive_motor.configure(drive_motor_config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters)
+        # closedLoop is the accessor for the closed loop controller configuration
+        drive_motor_config.closedLoop.pid(self.drive_pid.kP, self.drive_pid.kI,
+                                          self.drive_pid.kD, rev.ClosedLoopSlot.kSlot0)
+        self.drive_motor.configure(drive_motor_config, SparkBase.ResetMode.kResetSafeParameters,
+                                   SparkBase.PersistMode.kPersistParameters)
 
         self._swerve_controller = self.swerve_motor.getClosedLoopController()
         self._drive_controller = self.drive_motor.getClosedLoopController()
+
 
     def set_swerve_rotations(self, rot):
         self._swerve_controller.setReference(rot, SparkLowLevel.ControlType.kMAXMotionPositionControl, rev.ClosedLoopSlot.kSlot0, 1 / self.swerve_kV)
